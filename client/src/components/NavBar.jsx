aef7693c843c615f3cec113975a83be6c316d8c1
Bibin Graceson J <118437373+bibinwithab@users.noreply.github.com>
2023-10-27 11:54:57 +0530

    navBar

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -3,7 +3,13 @@ import { Link } from "react-router-dom"
 const NavBar = () => {
   return (
     <>
-        <nav>
+        <nav style={{
+            display: 'flex',
+            justifyContent: 'space-around',
+            backgroundColor: 'lightgrey',
+            padding: '10px',
+            margin: '10px',
+        }}>
             <Link to='/'>Home</Link>
             <Link to='/dashboard'>Dashboard</Link>
             <Link to='/userprofile'>User Profile</Link>
